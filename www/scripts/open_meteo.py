c306fed864021cc29e62ab91b3a77d4e91c57c2c
vtspaid <vtspaid@gmail.com>
2025-11-07 16:54:52 -0500

    Made start date optional. First date of the year of the end date is chosen by default

--- www/scripts/open_meteo.py
+++ www/scripts/open_meteo.py
@@ -1,5 +1,6 @@
 
 import openmeteo_requests
+import datetime
 
 import pandas as pd
 import requests_cache
@@ -9,8 +10,11 @@ cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
-def get_daily_temps(lat, long, start, end):
+def get_daily_temps(lat, long, start=None, end):
 
+    if start is None:
+        year = datetime.datetime.strptime(my_date, "%Y-%m-%d").year
+        start = str(year) + "-01-01"
     # Make sure all required weather variables are listed here
     # The order of variables in hourly or daily is important to assign them correctly below
     url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
